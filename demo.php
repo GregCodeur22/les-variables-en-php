6e89c34fc2dd2aaeaf3e7966170dead1e30c6f10
Grégory <gregory.arside@gmail.com>
2022-04-28 13:58:29 +0200

    début de tableau

--- demo.php
+++ demo.php
@@ -1,10 +1,23 @@
 <?php
-$prenom = 'Marc';
-$nom = 'Doe';
-$note1 = 10;
-$note2 = 20;
+// $prenom = 'Marc';
+// $nom = 'Doe';
+// $note1 = 10;
+// $note2 = 20;
 
-echo "Bonjour $prenom $nom vous avez eu " . ($note1 + $note2)/2 ." de moyenne";
+// echo "Bonjour $prenom $nom vous avez eu " . ($note1 + $note2)/2 ." de moyenne";
 
-$notes = [10, 20, 23, 56, 5];
-echo $notes[3];
+// $notes = [10, 20, 23, 56, 5];
+
+// echo $notes[3];
+
+$eleves = [
+    'nom' => 'Doe',
+    'prenom' => 'Marc',
+    'notes' => [10, 20, 15]
+];
+
+$eleves['prenom'] = 'Jean' ;
+
+echo $eleves['nom'] . ' '. $eleves['prenom'];
+
+print_r($eleves['notes']);
